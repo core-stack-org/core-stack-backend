b1ae38b74b00e1af682e9515301fd9e5fe058c9d
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-03-25 12:32:50 +0530

    type/changes_for_excel_for_handling_exception

--- computing/admin.py
+++ computing/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LayerInfo
 
 # Register your models here.
+admin.site.register(LayerInfo)
--- computing/models.py
+++ computing/models.py
@@ -32,3 +32,28 @@ class GeneratedLayerInfo(models.Model):
     class Meta:
         ordering = ['-created_at']
 
+
+class LayerInfo(models.Model):
+    LAYER_TYPE_CHOICES = [
+        ("raster", "Raster"),
+        ("vector", "Vector"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    layer_name = models.CharField(max_length=255)
+    layer_type = models.CharField(max_length=50, choices=LAYER_TYPE_CHOICES)
+    workspace = models.CharField(max_length=255, blank=True, null=True)
+    layer_desc = models.TextField(blank=True, null=True)
+    excel_to_be_generated = models.BooleanField(default=False)
+    start_year = models.PositiveIntegerField(blank=True, null=True)
+    end_year = models.PositiveIntegerField(blank=True, null=True)
+    style_name = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.workspace + '/' + self.layer_name
+
+    class Meta:
+        ordering = ['-created_at']
+
