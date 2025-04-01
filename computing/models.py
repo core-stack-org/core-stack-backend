6695557ab5dfd79b7fdefd8218509a76026f0c7a
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-04-01 16:13:20 +0530

    exception handling for the excel

--- computing/models.py
+++ computing/models.py
@@ -1,7 +1,36 @@
 from django.db import models
+from geoadmin.models import State, District, Block
+
 
 # Create your models here.
 
 # create a dummy MWS model with mws id
 class MWS:
     id = models.IntegerField(primary_key=True)
+
+# Models for excel generation
+class LayerInfo(models.Model):
+    LAYER_TYPE_CHOICES = [
+        ("raster", "Raster"),
+        ("vector", "Vector"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    layer_name = models.CharField(max_length=255)
+    layer_type = models.CharField(max_length=50, choices=LAYER_TYPE_CHOICES)
+    workspace = models.CharField(max_length=255, blank=True, null=True)
+    layer_desc = models.TextField(blank=True, null=True)
+    excel_to_be_generated = models.BooleanField(default=False)
+    start_year = models.PositiveIntegerField(blank=True, null=True)
+    end_year = models.PositiveIntegerField(blank=True, null=True)
+    style_name = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.workspace + '/' + self.layer_name
+
+    class Meta:
+        ordering = ['-created_at']
+
+
--- nrm_app/settings.py
+++ nrm_app/settings.py
@@ -175,6 +175,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 ASSET_DIR = "/home/ubuntu/prod_dir/nrm-app/assets/"
+EXCEL_PATH = "/home/ubuntu/cfpt/core-stack-backend/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
