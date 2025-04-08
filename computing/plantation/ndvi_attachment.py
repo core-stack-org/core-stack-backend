c0cadee1784ced41e66b3f5e8afb23c34805da34
aman verma <aman.verma@oniondev.com>
2025-04-08 16:41:38 +0530

    harmonized ndvi in site suitability

--- computing/plantation/ndvi_attachment.py
+++ computing/plantation/ndvi_attachment.py
@@ -64,7 +64,7 @@ def get_ndvi_data(suitability_vector, start_year, end_year):
             """
             # Calculate mean NDVI for the feature's geometry
             mean_ndvi = image.reduceRegion(
-                reducer=ee.Reducer.mean(), geometry=feature.geometry(), scale=10
+                reducer=ee.Reducer.mean(), geometry=feature.geometry(), scale=30
             ).get("NDVI")
 
             # Get the date and extract the year
