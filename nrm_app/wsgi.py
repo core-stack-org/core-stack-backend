1424ecdc307ce214e6e085e0c55e005ef39b7072
Ankit K. <ankit.kumar@oniondev.com>
2025-04-29 14:14:46 +0530

    GDAL hard coded path for data dir

--- nrm_app/wsgi.py
+++ nrm_app/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nrm_app.settings")
 
+os.environ['GDAL_DATA'] = '/home/ubuntu/prod_dir/nrm-app/venv/envs/corestack/share/gdal'
+os.environ['LD_LIBRARY_PATH'] = '/home/ubuntu/prod_dir/nrm-app/venv/envs/corestack/lib'
+
 application = get_wsgi_application()
