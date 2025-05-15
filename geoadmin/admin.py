586e442ab81ecd961196fbd22f0131925b855ecc
oniondev_developers <shiv.prakash@oniondev.com>
2025-05-15 14:24:40 +0530

    soi_narega_state_dist_block_db

--- geoadmin/admin.py
+++ geoadmin/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import State, District, Block
+from .models import State, District, Block, State_SOI, District_SOI, Block_SOI
 
 # Register your models here.
 admin.site.register(State)
 admin.site.register(District)
 admin.site.register(Block)
+admin.site.register(State_SOI)
+admin.site.register(District_SOI)
+admin.site.register(Block_SOI)
