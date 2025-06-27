23179c42234be3c99ea6fd745203f5e8193ccf5d
Ankit K. <ankit.kumar@oniondev.com>
2025-06-27 17:05:24 +0530

    Sync offline data for work fix.

--- plans/urls.py
+++ plans/urls.py
@@ -16,12 +16,12 @@ urlpatterns = [
     path("add_resources/", api.add_resources, name="add_resources"),
     path("add_works/", api.add_works, name="add_works"),
     path(
-        "sync_offline_data/<str:resource_type>/",
+        "sync_offline_data/resource/<str:resource_type>/",
         api.sync_offline_data,
         name="sync_offline_data",
     ),
     path(
-        "sync_offline_data/<str:work_type>/",
+        "sync_offline_data/work/<str:work_type>/",
         api.sync_offline_data,
         name="sync_offline_data",
     ),
