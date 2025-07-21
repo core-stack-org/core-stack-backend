6ebad2ab873e55a5ab568c2162a07f2f0c160c17
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-07-21 15:06:24 +0530

    updated some lulc remain part

--- computing/lulc/misc.py
+++ computing/lulc/misc.py
@@ -131,7 +131,7 @@ def clip_lulc_from_river_basin(
         # Clipping river basin lulc for a particular geometry
         lulc = ee.Image(
             GEE_DATASET_PATH
-            + ("LULC_v2_river_basin/" if version == "v2" else "LULC_v3_river_basin/")
+            + ("/LULC_v2_river_basin/" if version == "v2" else "/LULC_v3_river_basin/")
             + lulc_name
         ).clip(roi.geometry())
     return lulc
