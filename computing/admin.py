21dc4a38d213edad5894559da32b87a65ff7938c
pawangramvaani <pawa.soni@gramvaani.org>
2025-08-18 11:28:41 +0530

    minor fixes

--- computing/admin.py
+++ computing/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
+
 
 @admin.register(Layer)
 class LayerAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at', 'updated_at')
-    search_fields = ("layer_name",)
+    # readonly_fields = ("created_at", "updated_at")
+    # search_fields = ("layer_name", "dataset")
+    list_display = ("layer_name", "dataset__workspace", "dataset__name")
+    # list_filter = ("is_public_gee_asset", "block")
+
 
 @admin.register(Dataset)
 class LayerAdmin(admin.ModelAdmin):
     search_fields = ("name",)
-
-
