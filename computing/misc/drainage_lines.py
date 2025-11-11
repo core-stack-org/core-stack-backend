18e21ee3418b3028acfbb41948bdfc6931e68a42
Nirzaree <nirzaree.svnit@gmail.com>
2025-11-11 17:51:53 +0530

    updating drainage lines pipeline with stac integration

--- computing/misc/drainage_lines.py
+++ computing/misc/drainage_lines.py
@@ -17,7 +17,7 @@ from utilities.gee_utils import (
 )
 from utilities.constants import GEE_DATASET_PATH
 from nrm_app.celery import app
-
+from computing.STAC_specs import generate_STAC_layerwise
 
 @app.task(bind=True)
 def clip_drainage_lines(
@@ -75,6 +75,16 @@ def clip_drainage_lines(
             if res["status_code"] == 201 and layer_id:
                 update_layer_sync_status(layer_id=layer_id, sync_to_geoserver=True)
                 print("sync to geoserver flag is updated")
+
+                layer_STAC_generated = False
+                layer_STAC_generated = generate_STAC_layerwise.generate_vector_stac(
+                    state=state,
+                    district=district,
+                    block=block,
+                    layer_name='drainage_lines_vector')
+                update_layer_sync_status(layer_id=layer_id,
+                                         is_stac_specs_generated=layer_STAC_generated)
+                
                 layer_at_geoserver = True
 
         except Exception as e:
