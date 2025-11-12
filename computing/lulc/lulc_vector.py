66867000a9771acf395ca32a3050b4a9cc6d6b48
Nirzaree <nirzaree.svnit@gmail.com>
2025-11-12 22:43:36 +0530

    removing stac from lulc vector layer as it does not have any style file

--- computing/lulc/lulc_vector.py
+++ computing/lulc/lulc_vector.py
@@ -16,7 +16,7 @@ from utilities.gee_utils import (
 )
 from nrm_app.celery import app
 from computing.models import *
-from computing.STAC_specs import generate_STAC_layerwise
+# from computing.STAC_specs import generate_STAC_layerwise
 
 @app.task(bind=True)
 def vectorise_lulc(self, state, district, block, start_year, end_year, gee_account_id):
@@ -221,15 +221,15 @@ def sync_to_db_and_geoserver(
             print("sync to geoserver flag updated")
 
             #stac specs generation block
-            layer_STAC_generated = False
-            layer_STAC_generated = generate_STAC_layerwise.generate_vector_stac(
-                state=state,
-                district=district,
-                block=block,
-                layer_name='land_use_land_cover_vector')
+            # layer_STAC_generated = False
+            # layer_STAC_generated = generate_STAC_layerwise.generate_vector_stac(
+            #     state=state,
+            #     district=district,
+            #     block=block,
+            #     layer_name='land_use_land_cover_vector')
             
-            update_layer_sync_status(layer_id=layer_id,
-                                 is_stac_specs_generated=layer_STAC_generated)
+            # update_layer_sync_status(layer_id=layer_id,
+            #                      is_stac_specs_generated=layer_STAC_generated)
             
             layer_at_geoserver = True
             
