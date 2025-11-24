eff5a26513a881cf8321e35c70f443628c12c8e6
pawangramvaani <pawan.soni@gramvaani.org>
2025-11-24 15:13:12 +0530

    add list display

--- computing/admin.py
+++ computing/admin.py
@@ -8,8 +8,17 @@ from .models import *
 class LayerAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", "updated_at")
     search_fields = ("layer_name",)
+    list_display = ["misc", "state", "dataset", "layer_name", "layer_version"]
+    list_filter = [
+        "is_stac_specs_generated",
+        "is_sync_to_geoserver",
+        "layer_version",
+        "dataset",
+    ]
 
 
 @admin.register(Dataset)
 class LayerAdmin(admin.ModelAdmin):
     search_fields = ("name",)
+    list_display = ["layer_type", "name", "workspace"]
+    list_filter = ["layer_type"]
