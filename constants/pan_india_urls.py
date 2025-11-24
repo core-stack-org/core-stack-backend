42c85a0487608097a0cc20a611c0c5fbcd7854e6
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-11-24 14:34:52 +0530

    updated code for ddl and soge

--- constants/pan_india_urls.py
+++ constants/pan_india_urls.py
@@ -1,3 +1,6 @@
 CATCHMETN_AREA = "projects/ext-datasets/assets/datasets/catchment_area_singleflow"
 NATURAL_DEPRESSION = "projects/ext-datasets/assets/datasets/Natural_depression_raster"
 SLOPE_PERCENTAGE = "projects/ext-datasets/assets/datasets/slope_percentage_fabdem"
+DISTANCE_TO_UPSTREAM_DL = (
+    "projects/ext-datasets/assets/datasets/distance_to_nearest_upstream_DL_raster"
+)
