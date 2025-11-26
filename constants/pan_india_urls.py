7d14bd2a31dc3d9e8ec78ddf6134a6df98e54ef3
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-11-26 13:42:37 +0530

    Pipeline for the tree health

--- constants/pan_india_urls.py
+++ constants/pan_india_urls.py
@@ -1,4 +1,4 @@
-  CATCHMETN_AREA = "projects/ext-datasets/assets/datasets/catchment_area_singleflow"
+CATCHMETN_AREA = "projects/ext-datasets/assets/datasets/catchment_area_singleflow"
 NATURAL_DEPRESSION = "projects/ext-datasets/assets/datasets/Natural_depression_raster"
 SLOPE_PERCENTAGE = "projects/ext-datasets/assets/datasets/slope_percentage_fabdem"
 DISTANCE_TO_UPSTREAM_DL = (
