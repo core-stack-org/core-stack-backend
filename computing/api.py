d383a5607452c385d1bb4fdcba8de85acec50c20
shiv.prakash1 <shiv.prakash@oniondev.com>
2025-12-03 12:21:04 +0530

    changes for tree_health and drainage

--- computing/api.py
+++ computing/api.py
@@ -118,7 +118,13 @@ def generate_drainage_layer(request):
         block = request.data.get("block").lower()
         gee_account_id = request.data.get("gee_account_id")
         clip_drainage_lines.apply_async(
-            args=[state, district, block, gee_account_id], queue="nrm"
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "gee_account_id": gee_account_id,
+            },
+            queue="nrm",
         )
         return Response(
             {"Success": "Successfully initiated"}, status=status.HTTP_200_OK
@@ -754,16 +760,37 @@ def tree_health_raster(request):
         end_year = request.data.get("end_year")
         gee_account_id = request.data.get("gee_account_id")
         tree_health_ccd_raster.apply_async(
-            args=[state, district, block, start_year, end_year, gee_account_id],
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "start_year": start_year,
+                "end_year": end_year,
+                "gee_account_id": gee_account_id,
+            },
             queue="nrm",
         )
         tree_health_ch_raster.apply_async(
-            args=[state, district, block, start_year, end_year, gee_account_id],
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "start_year": start_year,
+                "end_year": end_year,
+                "gee_account_id": gee_account_id,
+            },
             queue="nrm",
         )
         tree_health_overall_change_raster.apply_async(
-            args=[state, district, block, gee_account_id], queue="nrm"
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "gee_account_id": gee_account_id,
+            },
+            queue="nrm",
         )
+
         return Response(
             {"Success": "tree_health task initiated"},
             status=status.HTTP_200_OK,
@@ -785,14 +812,34 @@ def tree_health_vector(request):
         end_year = request.data.get("end_year")
         gee_account_id = request.data.get("gee_account_id")
         tree_health_overall_change_vector.apply_async(
-            args=[state, district, block, gee_account_id], queue="nrm"
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "gee_account_id": gee_account_id,
+            },
+            queue="nrm",
         )
         tree_health_ch_vector.apply_async(
-            args=[state, district, block, start_year, end_year, gee_account_id],
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "start_year": start_year,
+                "end_year": end_year,
+                "gee_account_id": gee_account_id,
+            },
             queue="nrm",
         )
         tree_health_ccd_vector.apply_async(
-            args=[state, district, block, start_year, end_year, gee_account_id],
+            kwargs={
+                "state": state,
+                "district": district,
+                "block": block,
+                "start_year": start_year,
+                "end_year": end_year,
+                "gee_account_id": gee_account_id,
+            },
             queue="nrm",
         )
         return Response(
--- computing/tree_health/canopy_height.py
+++ computing/tree_health/canopy_height.py
@@ -1,5 +1,6 @@
 import ee
 from nrm_app.celery import app
+from utilities.constants import GEE_PATHS
 from utilities.gee_utils import (
     ee_initialize,
     valid_gee_text,
@@ -10,153 +11,114 @@ from utilities.gee_utils import (
     sync_raster_gcs_to_geoserver,
     export_raster_asset_to_gee,
     make_asset_public,
+    get_gee_dir_path,
 )
 from computing.utils import save_layer_info_to_db, update_layer_sync_status
 from computing.STAC_specs import generate_STAC_layerwise
+from constants.pan_india_urls import CH_RASTER, CH_RASTER_WITHOUT_MODEL
 
 
 @app.task(bind=True)
 def tree_health_ch_raster(
-        self, state, district, block, start_year, end_year, gee_account_id
+    self,
+    state=None,
+    district=None,
+    block=None,
+    roi=None,
+    asset_suffix=None,
+    asset_folder_list=None,
+    start_year=None,
+    end_year=None,
+    app_type="MWS",
+    gee_account_id=None,
 ):
+    print("Inside process Tree health ch raster")
     ee_initialize(gee_account_id)
-    print("Inside process tree_health_ch_raster")
-    # ch_palette = [
-    #     "FFA500",
-    #     "FFA500",
-    #     "DEE64C",
-    #     "DEE64C",
-    #     "DEE64C",
-    #     "DEE64C",
-    #     "007500",
-    #     "007500",
-    #     "000000"
-    # ]
 
-    block_mws = ee.FeatureCollection(
-        get_gee_asset_path(state, district, block)
-        + "filtered_mws_"
-        + valid_gee_text(district.lower())
-        + "_"
-        + valid_gee_text(block.lower())
-        + "_uid"
-    )
+    if state and district and block:
+        asset_suffix = (
+            valid_gee_text(district.lower()) + "_" + valid_gee_text(block.lower())
+        )
+        asset_folder_list = [state, district, block]
+
+        roi = ee.FeatureCollection(
+            get_gee_dir_path(
+                asset_folder_list, asset_path=GEE_PATHS[app_type]["GEE_ASSET_PATH"]
+            )
+            + "filtered_mws_"
+            + asset_suffix
+            + "_uid"
+        )
+
     layer_at_geoserver = False
-    for year in range(start_year, end_year):
+    for year in range(start_year, end_year + 1):
         description = (
-            "tree_health_ch_raster_"
+            "ch_raster_"
             + valid_gee_text(district.lower())
             + "_"
             + valid_gee_text(block.lower())
             + "_"
             + str(year)
         )
-        asset_id = get_gee_asset_path(state, district, block) + description
 
-        if is_gee_asset_exists(asset_id):
-            return True
-
-        # Define the path for CH data based on start year
-        if year == 2016 or year == 2022:
-            ch_path = "projects/ee-mtpictd/assets/harsh/ch_" + str(year)
-        else:
-            ch_path = "projects/ee-mtpictd/assets/dhruvi/modal_ch_" + str(year)
-
-        # Load and process the CH Image Collection
-        ch_img = (
-            ee.ImageCollection(ch_path).filterBounds(block_mws).mean().clip(block_mws)
+        asset_id = (
+            get_gee_dir_path(
+                asset_folder_list, asset_path=GEE_PATHS[app_type]["GEE_ASSET_PATH"]
+            )
+            + description
         )
 
-        # visualization = {"min": 0, "max": 8, "palette": ch_palette}
+        if not is_gee_asset_exists(asset_id):
+            if year == 2023:
+                ch_raster = ee.ImageCollection(CH_RASTER_WITHOUT_MODEL)
+            else:
+                ch_raster = ee.ImageCollection(CH_RASTER + str(year))
+            raster = ch_raster.filterBounds(roi.geometry()).mean().clip(roi.geometry())
 
-        ch_composite = ch_img.rename(["classification"])
-
-        # Function to compute the change statistics per block
-        def change_stats(feature):
-            stats = ch_composite.reduceRegion(
-                reducer=ee.Reducer.frequencyHistogram().unweighted(),
-                geometry=feature.geometry(),
+            task_id = export_raster_asset_to_gee(
+                image=raster,
+                description=description,
+                asset_id=asset_id,
                 scale=25,
-                maxPixels=1e10,
+                region=roi.geometry(),
             )
-            pixelCounts = ee.Dictionary(stats.get("classification"))
-
-            # Return feature with additional properties representing the change statistics
-            return feature.set(
-                {
-                    "ch_0": ee.Number(pixelCounts.get("0.0", 0)).add(
-                        ee.Number(pixelCounts.get("1.0", 0))
-                    ),  # Short Trees
-                    "ch_1": ee.Number(pixelCounts.get("2.0", 0))
-                    .add(ee.Number(pixelCounts.get("3.0", 0)))
-                    .add(ee.Number(pixelCounts.get("4.0", 0)))
-                    .add(ee.Number(pixelCounts.get("5.0", 0))),  # Medium Height Trees
-                    "ch_2": ee.Number(pixelCounts.get("6.0", 0)).add(
-                        ee.Number(pixelCounts.get("7.0", 0))
-                    ),  # Tall Trees
-                    "ch_3": pixelCounts.get("8.0", 0),  # Missing Data
-                }
-            )
-
-        block_mws = block_mws.map(change_stats)
-
-        # Export the result to GEE
-        task_id = export_raster_asset_to_gee(
-            image=ch_img.clip(block_mws.geometry()),
-            description=description,
-            asset_id=asset_id,
-            scale=25,
-            region=block_mws.geometry(),
-        )
-        task_id_list = check_task_status([task_id])
-        print("CH task_id_list", task_id_list)
+            task_id_list = check_task_status([task_id])
+            print("CH task_id_list", task_id_list)
 
         if is_gee_asset_exists(asset_id):
             make_asset_public(asset_id)
-            layer_name = (
-                "tree_health_ch_raster_"
-                + valid_gee_text(district.lower())
-                + "_"
-                + valid_gee_text(block.lower())
-                + "_"
-                + str(year)
+            layer_id = save_layer_info_to_db(
+                state,
+                district,
+                block,
+                description,
+                asset_id,
+                "Canopy Height Raster",
+                misc={"start_year": start_year, "end_year": end_year},
             )
-
-            # layer_id = save_layer_info_to_db(
-            #     state,
-            #     district,
-            #     block,
-            #     layer_name,
-            #     asset_id,
-            #     "Canopy Height Raster",
-            #     misc={"start_year": start_year, "end_year": end_year},
-            # )
-
-            # Sync image to Google Cloud Storage and Geoserver
-            task_id = sync_raster_to_gcs(ee.Image(asset_id), 30, layer_name)
+            task_id = sync_raster_to_gcs(ee.Image(asset_id), 25, description)
 
             task_id_list = check_task_status([task_id])
             print("task_id_list sync to GCS", task_id_list)
 
+            make_asset_public(asset_id)
             res = sync_raster_gcs_to_geoserver(
-                "canopy_height", layer_name, layer_name, "ch_style"
+                "canopy_height", description, description, "ch_style"
             )
 
             # if res and layer_id:
-            #     update_layer_sync_status(layer_id=layer_id, sync_to_geoserver=True)
-            #     print("sync to geoserver flag is updated")
-            if res:
-                # stac specs generation block
-                layer_STAC_generated = False
-                layer_STAC_generated = generate_STAC_layerwise.generate_raster_stac(
-                    state=state,
-                    district=district,
-                    block=block,
-                    layer_name="tree_canopy_height_raster",
-                    start_year=year,
-                )
-
-                # update_layer_sync_status(layer_id=layer_id,
-                #                          is_stac_specs_generated=layer_STAC_generated)
-                layer_at_geoserver = True
+            #     layer_at_geoserver = True
+            #     layer_STAC_generated = False
+            #     layer_STAC_generated = generate_STAC_layerwise.generate_raster_stac(
+            #         state=state,
+            #         district=district,
+            #         block=block,
+            #         layer_name="ch_raster",
+            #         start_year=year,
+            #     )
+            #     update_layer_sync_status(
+            #         layer_id=layer_id,
+            #         is_stac_specs_generated=layer_STAC_generated,
+            #         sync_to_geoserver=layer_at_geoserver,
+            #     )
     return layer_at_geoserver
