973e92f5a6b320f28a5de0f2092baa52b55af7df
Ankit Kumar <ankit.kumar@oniondev.com>
2026-02-15 17:09:12 +0530

    update to the admin pages of dpr and moderation

--- moderation/admin.py
+++ moderation/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SyncMetadata
+
+
+@admin.register(SyncMetadata)
+class SyncMetadataAdmin(admin.ModelAdmin):
+    list_display = ["sync_type", "last_synced_at", "baseline_date"]
+    list_filter = ["sync_type"]
+    search_fields = ["sync_type"]
+    readonly_fields = ["sync_type", "last_synced_at", "baseline_date"]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("sync_type", "last_synced_at", "baseline_date"),
+            },
+        ),
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
