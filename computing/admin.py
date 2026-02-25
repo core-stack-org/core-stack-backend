e43ad4c1dea84176b6b048d21514ddb2648b4e6c
Abhishek <abhishekup082gmail.com@Abhisheks-MacBook-Air.local>
2026-02-26 01:16:13 +0530

    fix the issue

--- computing/admin.py
+++ computing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from .drought.models import DroughtAlert
 
 # Register your models here.
 
@@ -18,7 +19,25 @@ class LayerAdmin(admin.ModelAdmin):
 
 
 @admin.register(Dataset)
-class LayerAdmin(admin.ModelAdmin):
+class DatasetAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_display = ["name", "layer_type", "workspace"]
     list_filter = ["layer_type"]
+
+
+@admin.register(DroughtAlert)
+class DroughtAlertAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "severity",
+        "alert_type",
+        "aoi_name",
+        "state",
+        "district",
+        "alert_date",
+        "is_active",
+    ]
+    list_filter = ["severity", "alert_type", "is_active", "state"]
+    search_fields = ["aoi_name", "state", "district", "block"]
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ["-alert_date", "-created_at"]
