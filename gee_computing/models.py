5087e5a2942c5887261907ba357f670f3acacd90
pawangramvaani <pawan.soni@gramvaani.org>
2026-03-27 11:15:19 -0700

    rearrange columns

--- gee_computing/models.py
+++ gee_computing/models.py
@@ -5,9 +5,9 @@ from nrm_app.settings import FERNET_KEY
 
 class GEEAccount(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    service_account_email = models.EmailField()
     account_name = models.CharField(max_length=100, unique=True, blank=True, null=True)
     service_account_name = models.CharField(max_length=100, blank=True, null=True)
+    service_account_email = models.EmailField()
     helper_account = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
