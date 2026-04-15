41308ea597535b8dfa751aca1d47a1f622ed7660
Ankit K <ankit.kumar@oniondev.com>
2026-04-16 00:33:29 +0530

    added methods to update db and generate change detection from local machine

--- computing/apps.py
+++ computing/apps.py
@@ -8,3 +8,6 @@ class ComputingConfig(AppConfig):
     def ready(self):
         import computing.tasks  # noqa: F401  (task autodiscovery)
         import computing.signals  # noqa: F401  (post_save -> auto STAC trigger)
+        from computing.base_layer_setup import setup_base_layers
+
+        setup_base_layers()
