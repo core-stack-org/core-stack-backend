49a6a73643fdf7b273eaa069ae4cba3c82fcfb27
pawangramvaani <pawan.soni@gramvaani.org>
2026-06-24 18:06:12 +0530

    Features/missing excel and json (#1043)
    * checking for excel missing
    
    * check xlsx sheet also
    
    * rebase dev
    
    * configure email functionality
    
    * add excel generation in json map
    
    * move allowed host to .env
    
    * add non mandatory sheet
    
    * handle partial data sheet
    
    * move constants to utilities
    
    * handle conditional sheets
    
    * generate excel post layer
    
    * Feat/livestocks data processing docs schema (#1027)
    
    * Add livestock dataset preparation script (cleaning, structuring raw census files)
    
    * Add livestock data analysis script (basic metrics, validation, summary outputs)
    
    * Finalize livestock documentation (metadata, usage notes, analysis recommendations)
    
    * Minor edit in livestock README
    
    * corrected database name, CS-dev GPKG link.
    
    * rebase dev
    
    * change mail body
    
    * sheet name read from models
    
    * rename model field name
    
    ---------
    
    Co-authored-by: Amit Kumar <amit.kumar@gramvaani.org>

--- computing/api.py
+++ computing/api.py
@@ -74,6 +74,7 @@ from .views import (
     get_layers_of_workspace,
     missing_layer_for_all_workspace,
     clear_layer_cache,
+    check_missing_excel_files,
 )
 from .misc.lcw_conflict import generate_lcw_conflict_data
 from .misc.agroecological_space import generate_agroecological_data
@@ -1885,7 +1886,7 @@ def missing_layers(request):
         result = missing_layer_for_all_workspace()
         return Response({"result": result}, status=status.HTTP_200_OK)
     except Exception as e:
-        print("Exception in get_layers_for_workspace api :: ", e)
+        print("Exception in missing_layers api :: ", e)
         return Response({"Exception": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
@@ -1938,4 +1939,15 @@ def generate_canal_vector(request):
         print(
             f"Exception in generate canal vector layer for {district} - {block}:: ", e
         )
+        return Response({"Exception": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+@schema(None)
+def missing_excel(request):
+    try:
+        result = check_missing_excel_files()
+        return Response({"result": result}, status=status.HTTP_200_OK)
+    except Exception as e:
+        print("Exception in missing_excel api :: ", e)
         return Response({"Exception": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
